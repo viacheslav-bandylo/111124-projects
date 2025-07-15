fee44a954fcf1c8ece923b3c37b6ba42cce7e9c3
Viacheslav Bandylo <viacheslav.bandylo@gmail.com>
2025-07-15 11:45:55 +0300

    Lesson 25

--- library/views.py
+++ library/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.pagination import PageNumberPagination
 from .models import Book
 from .serializers import BookListSerializer, BookDetailSerializer, BookCreateSerializer, GenreSerializer
 
@@ -20,11 +21,51 @@ from .serializers import BookListSerializer, BookDetailSerializer, BookCreateSer
 #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
-class BookListCreateView(APIView):
+class BookListCreateView(APIView, PageNumberPagination):
+    page_size = 2
+
     def get(self, request):
-        books = Book.objects.all()
-        serializer = BookListSerializer(books, many=True)
-        return Response(serializer.data)
+        #### Filter ####
+        filters = {}
+        title = request.query_params.get('title')
+        published_year = request.query_params.get('pub_year')
+
+        if title:
+            filters['title'] = title
+
+        if published_year:
+            filters['publication_date__year'] = published_year
+
+        books = Book.objects.filter(**filters)
+
+        #### Sorting ####
+
+        # Получаем параметр 'sort_by'. Если его нет, по умолчанию сортируем по 'title'.
+        sort_by = request.query_params.get('sort_by', 'title')
+        # Получаем параметр 'sort_order'. По умолчанию сортируем по возрастанию ('asc').
+        sort_order = request.query_params.get('sort_order', 'asc')
+
+        if sort_order == 'desc': # Если порядок сортировки 'desc' (убывание)...
+            sort_by = f'-{sort_by}' # ...добавляем минус перед именем поля для убывающей сортировки
+        books = books.order_by(sort_by) # Применяем сортировку
+
+        #### Pagination ####
+
+        requested_page_size = self.get_page_size(request)
+        self.page_size = requested_page_size
+
+        result = self.paginate_queryset(books, request, view=self)
+
+        serializer = BookListSerializer(result, many=True)
+        return self.get_paginated_response(serializer.data)
+    
+    def get_page_size(self, request):
+        page_size_param = request.query_params.get('page_size')
+
+        if page_size_param and page_size_param.isdigit():
+            return int(page_size_param)
+        
+        return self.page_size
     
     def post(self, request):
         serializer = BookCreateSerializer(data=request.data)
